9a32a6dbef568e0b2af85d7e45e977100bffd552
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-01-19 00:05:05 +0100

    Removing unique restriction from BookVolume as still heroku complains - session transaction rolled back

--- models/book_volume.py
+++ models/book_volume.py
@@ -11,7 +11,7 @@ class BookVolume(db.Model):
     __tablename__ = 'book_volume'
     id = db.Column(db.Integer(), primary_key=True)
     kind = db.Column(db.String(100))
-    internal_id = db.Column(db.String(100), unique=True)
+    internal_id = db.Column(db.String(100)) #, unique=True)
     etag = db.Column(db.String(100))
     selfLink = db.Column(db.String(250))
     created_at = db.Column(db.DateTime())
